b8638d4e9e701213c0d68b1c57c8b7630f2ce094
Gabriel Silva <dosanjosg22@gmail.com>
2024-03-17 17:17:41 -0400

    Removido o parametro velocidadeVento e modificado indices de umidade

--- main.c
+++ main.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
-void PrevisaoDeQueimada (float velocidadeVento, float valorTemperatura, int valorUmidade);
+void PrevisaoDeQueimada (float valorTemperatura, int valorUmidade);
 
 int main () {
 
-    float velocidadeVento, valorTemperatura;
+    float valorTemperatura;
     int valorUmidade;
 
     printf("Entre com valor da temperatura registrada: ");
     scanf("%f", &valorTemperatura);
-    printf("Entre com o valor da velocidade registrada dos ventos: ");
-    scanf("%f", &velocidadeVento);
-    printf("Classifique a umidade relativ (1 = Baixa / 2 = Moderada / 3 = Alta): ");
+
+    printf("Classifique a umidade relativa do ar (1 = Baixa /  2 = Alta): ");
     scanf("%d", &valorUmidade);
 
-    PrevisaoDeQueimada(velocidadeVento, valorTemperatura, valorUmidade);
+    PrevisaoDeQueimada(valorTemperatura, valorUmidade);
 
     return 0;
 }
 
-void PrevisaoDeQueimada(float velocidadeVento, float valorTemperatura, int valorUmidade) {
-    if(valorTemperatura > 30.0 && valorUmidade == 1 && velocidadeVento > 35.0) {
+void PrevisaoDeQueimada(float valorTemperatura, int valorUmidade) {
+    if(valorTemperatura > 40.0 && (valorUmidade == 1 || valorUmidade == 2)) {
         printf("Probabilidade alta de queimada!");
     }
-    else if ((valorTemperatura > 15.0 && valorTemperatura < 30) && valorUmidade == 2 &&velocidadeVento <= 18.0) {
+    else if ((valorTemperatura > 15.0 && valorTemperatura <= 40) && (valorUmidade == 1 || valorUmidade == 2)) {
         printf("Probilidade media de queimada!");
     }
-    else if(valorTemperatura < 15.0 && valorUmidade == 3 && velocidadeVento <= 18.0) {
+    else if(valorTemperatura <= 15.0 && (valorUmidade == 1 || valorUmidade == 2 )) {
         printf("Probalidade baixa de queimada!");
     }
 }
